e468125146020ae3f0f2fb0462263b8b2a19b365
Lulu albuquerque <115746095+luanaalbuuquerque4@users.noreply.github.com>
2025-10-28 17:20:51 -0300

    Delete echonotas-backend-updated directory

--- echonotas-backend-updated/src/main/java/com/echonotas/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.echonotas.repository;
-
-import com.echonotas.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findByNameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrCourseContainingIgnoreCase(String name, String email, String course);
-}
